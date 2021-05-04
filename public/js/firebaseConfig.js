88e49f4d7c92601274c8c41e55e8ff4874a90b00
Anya Ji <aj592@cornell.edu>
2021-05-04 02:53:20 -0400

    gitignore

--- public/js/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyDVFNgeaaWeBSNdAXoieOHmeoEcMnST6Xc",
-  authDomain: "tangram-c997f.firebaseapp.com",
-  projectId: "tangram-c997f",
-  storageBucket: "tangram-c997f.appspot.com",
-  messagingSenderId: "828079104765",
-  appId: "1:828079104765:web:d8c5928395240394e0d494",
-};
-firebase.initializeApp(firebaseConfig);
